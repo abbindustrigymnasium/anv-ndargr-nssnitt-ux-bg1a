b28b3639ff31d99f2e34cc9fb6d126d67bb72313
abbmaldel <malte.dellby@abbgymnasiet.se>
2023-08-23 12:36:14 +0200

    minor fixing

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,7 @@
 
 async function randomSausage(id) {
     const myElement = document.getElementById(id);
-    let x = await fetch("https://sausage-api.vercel.app/api/random")
+    let x = await fetch("https://cors.io/?https://sausage-api.vercel.app/api/random")
     data = x.json()
     let y = await x.name;
     myElement.innerHTML = y;
