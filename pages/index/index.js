e23f99fa938f56f8d97c70478ce1399eb2d4bbda
abbmaldel <malte.dellby@abbgymnasiet.se>
2023-08-23 09:09:13 +0200

    minor changes

---
+++ pages/index/index.js
@@ -0,0 +1,9 @@
+
+async function randomSausage(id) {
+    const myElement = document.getElementById(id);
+    let x = await fetch("https://sausage-api.vercel.app/api/random")
+    data = x.json()
+    let y = await x.name;
+    myElement.innerHTML = y;
+  }
+  
